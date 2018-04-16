f1819340c7f1321f31bb502c5086a5a0a691b41f
Diego Juliao <diego.juliao@bizagi.com>
2018-04-16 15:22:40 -0500

    Setup store and initial state

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,36 @@
+import * as fromStore from './store';
+
 import { renderTodos } from './utils';
 
+/**
+ * The input where the user writes the new todo
+ */
 const input = document.querySelector('input') as HTMLInputElement;
+/**
+ * The button to add a new todo
+ */
 const button = document.querySelector('button') as HTMLButtonElement;
+/**
+ * The unsubscribe button
+ */
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
+/**
+ * An <ul> element to write down all the todos
+ */
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const store = new fromStore.Store({}, {
+  todos: [{
+    label: 'Eat Pizza', complete: false
+  }]
+});
+
+console.log(store.value);
 
 button.addEventListener(
   'click',
   () => {
+    // If it is only spaces, do nothing...
     if (!input.value.trim()) return;
 
     const payload = { label: input.value, complete: false };
---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+/**
+ * Store of the State Manager
+ */
+export class Store {
+  private subscribers: Function[];
+  private reducers: { [key: string]: Function };
+  private state: { [key: string]: any };
+
+  constructor(reducers = {}, initialState = {} ) {
+    this.state = initialState;
+  }
+
+  get value() {
+    return this.state;
+  }
+}
